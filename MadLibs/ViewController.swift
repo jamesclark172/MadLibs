81c316a775ad016297b97b19359de6f1fa401c46
jamesclark172 <james.clark172@yahoo.com>
2017-08-12 18:22:31 -0500

    Initial Commit

---
+++ MadLibs/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MadLibs
+//
+//  Created by Laureane Clark on 8/12/17.
+//  Copyright © 2017 Laureane Clark. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
